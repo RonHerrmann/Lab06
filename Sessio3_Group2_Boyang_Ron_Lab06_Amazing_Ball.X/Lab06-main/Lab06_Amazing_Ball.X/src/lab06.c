cb562a084c8af80f5b4dc7136d86451e70e301bf
RonHerrmann <84956778+RonHerrmann@users.noreply.github.com>
2022-07-13 19:14:48 +0200

    Update lab06.c

--- Sessio3_Group2_Boyang_Ron_Lab06_Amazing_Ball.X/Lab06-main/Lab06_Amazing_Ball.X/src/lab06.c
+++ Sessio3_Group2_Boyang_Ron_Lab06_Amazing_Ball.X/Lab06-main/Lab06_Amazing_Ball.X/src/lab06.c
@@ -26,6 +26,10 @@
 #define Null_degree 0.9
 #define Ninety_degree 1.5
 #define HundredEighty_degree 2.1
+#define soll 350;
+
+#define getErr(Soll, positionClean) (Soll - positionClean)
+#define getPR3(sampleRate) ((float) 12.8e6 * 1/sampleRate / 256)
 
 
 /*
@@ -42,6 +46,23 @@
  * Global Variables
  */
 
+uint8_t volatile dimension = 0;
+uint16_t volatile iInterrupt = 0;
+uint8_t volatile flagTouch = 0;
+uint8_t volatile flagServo = 0;
+uint16_t volatile x_positionClean;
+uint16_t volatile y_positionClean;
+uint16_t volatile x_position = 0;
+uint16_t volatile y_position = 0;
+uint16_t volatile x_positionPrevious = 0;
+uint16_t volatile y_positionPrevious = 0;
+uint16_t volatile x_positionPreviousClean = 0;
+uint16_t volatile y_positionPreviousClean = 0;
+
+int16_t volatile errX = 0;
+int16_t volatile errXPrevious = 0;
+int16_t volatile errY = 0;
+int16_t volatile errYPrevious = 0;
 
 
 /*
@@ -68,45 +89,6 @@ void initialize_timer(){
  IEC0bits.T1IE = 1; // Enable Interrupts
  T1CONbits.TON = 1; // Enable the Timers    
     
-    
-
-/****** setup Timer 3 *****/
- 
- 
-T3CONbits.TON = 0; // Disable the Timers
-T3CONbits.TCKPS = 0b10; // Set Prescaler 64
-T3CONbits.TCS = 0; // Set Clock Source (external = 1)
-T3CONbits.TGATE = 0; // Set Gated Timer Mode -> don't use gating //this line can be ignored, if TCS =  1 (have a look at the manual)  
-
-PR3 = 1000; // Set timer period 5ms (50 Hz):    // 1000= 5*10^-3 * 12.8*10^6 * 1/64 
-TMR3 = 0x00; // Reset Timer Values
- 
-/* Insert Priority here!!! */
-IPC2bits.T3IP = 0x02; // Set Interrupt Priority (actually Level 1)
- 
-IFS0bits.T3IF = 0; // Clear Interrupt Flags
-IEC0bits.T3IE = 1; // Enable Interrupts
-T3CONbits.TON = 1; // Enable the Timers   
-
-
-
-/***** setup Timer 4 ******/
-
-T4CONbits.TON = 0; // Disable the Timers
-T4CONbits.TCKPS = 0b10; // Set Prescaler 64
-T4CONbits.TCS = 0; // Set Clock Source (external = 1)
-T4CONbits.TGATE = 0; // Set Gated Timer Mode -> don't use gating //this line can be ignored, if TCS =  1 (have a look at the manual)  
-
-PR4 = 100; // Set timer period 0.5ms (5 Hz):    // 100= 0.5*10^-3 * 12.8*10^6 * 1/64 
-TMR4 = 0x00; // Reset Timer Values
- 
-/* Insert Priority here!!! */
-IPC6bits.T4IP = 0x03; // Set Interrupt Priority (actually Level 1)
- 
-IFS1bits.T4IF = 0; // Clear Interrupt Flags
-IEC1bits.T4IE = 1; // Enable Interrupts
-T4CONbits.TON = 1; // Enable the Timers  
-
 }
 
 /*
@@ -125,11 +107,46 @@ T4CONbits.TON = 1; // Enable the Timers
  * PD Controller
  */
 
+#define Kp 0.1
+#define Kd 0.01
+
+
+void PD_Conctroller_Axis(uint16_t err, uint16_t errPrevious, uint8_t servo){
+    
+    float dutyCycle = 0;
+    
+    dutyCycle = (float)((Kp * err + Kd * (errY - errPrevious)/0.02) + 1.5);
+    
+    lcd_locate(0, 5);
+    lcd_printf("DutyCycle = %3.2f", dutyCycle);
+    
+    set_dutyCycle(servo, dutyCycle);
+    
+}
+
 
 
 /*
  * Butterworth Filter N=1, Cutoff 3 Hz, sampling @ 50 Hz
  */
+
+#define b1 0.1602
+#define b2 0.1602
+#define a1 1
+#define a2 -0.6796
+
+void ButterworthFilter(){
+    
+    if(dimension == 0){  // x-Dimension
+      x_positionPreviousClean = x_positionClean;  
+      x_positionClean = b1 * x_position + b2 * x_positionPrevious - a2 * x_positionPreviousClean;
+    }
+    else if(dimension == 1){  // y-Dimension
+      y_positionPreviousClean = y_positionClean;
+      y_positionClean = b1 * y_position + b2 * y_positionPrevious - a2 * y_positionPreviousClean;
+    }   
+}
+
 
 /*
  * ISR code 
@@ -137,26 +154,22 @@ T4CONbits.TON = 1; // Enable the Timers
  * Note: Timer 2 is already used for servos
  */
 
-void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T1Interrupt(void)
+void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T1Interrupt(void)   // read  the touchscreen X and Y dimension (read either X or Y and than alternate between them)
 {   
     TOGGLELED(LED1_PORT);
     
+    flagTouch =1;
+    
+    if(iInterrupt == 1){
+      flagServo = 1; 
+      iInterrupt = 0;  
+    }
+    
+    iInterrupt ++;
     IFS0bits.T1IF = 0;      // clear interrupt service routine flag
 }
 
-void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T3Interrupt(void)
-{   
-    TOGGLELED(LED2_PORT);
-    
-    IFS0bits.T3IF = 0;      // clear interrupt service routine flag
-}
 
-void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T4Interrupt(void)
-{   
-    TOGGLELED(LED3_PORT);
-    
-    IFS1bits.T4IF = 0;      // clear interrupt service routine flag
-}
 
 /*
  * main loop
@@ -169,7 +182,7 @@ void main_loop()
     lcd_printf("Group2: Boyang & Ron");
     lcd_locate(0, 2);
     
-    // initialize timers
+   // initialize timers
     initialize_timer();
     
     // initialize touchscreen
@@ -178,22 +191,55 @@ void main_loop()
     // initialize servos
     servo_initialization(X);
     servo_initialization(Y);
-    __delay_ms(2000);
-   
+    //__delay_ms(2000);
+    
     
     while(TRUE) {
-         
-        uint16_t x_value;
-        uint16_t y_value;
         
-        changeDimension_touchscreen(X);
-        x_value = currentBallPosition();
-    
-        changeDimension_touchscreen(Y);
-        y_value = currentBallPosition();
-    
-        lcd_locate(0, 5);
-        lcd_printf("X/Y = %3u/%3u", x_value, y_value);
+        if(flagTouch == 1){     // 100Hz
+            switch(dimension) {
+                case 0: changeDimension_touchscreen(X);
+                        x_positionPrevious = x_position;
+                        x_position = currentBallPosition();
+//                        lcd_locate(0, 3);
+//                        lcd_printf("X gemessen = %3u", x_position)
+                        ButterworthFilter();
+                        lcd_locate(0, 3);
+                        lcd_printf("X = %3u", x_positionClean); 
+                        dimension = 1; break;
+                case 1: changeDimension_touchscreen(Y);
+                        y_positionPrevious = y_position;
+                        y_position = currentBallPosition();
+//                        lcd_locate(0, 6);
+//                        lcd_printf("Y gemessen = %3u", y_position)
+                        ButterworthFilter();
+                        lcd_locate(0, 4);
+                        lcd_printf("Y = %3u", y_positionClean);
+                        dimension = 0; break;
+
+                }
+            flagTouch = 0;
+        } 
         
-    }
+         if(flagServo == 1){  //50 Hz
+             
+             errX = getErr(350, x_positionClean);
+             lcd_locate(0, 6);
+             lcd_printf("ErrX = %04d", errX);
+             
+             PD_Conctroller_Axis(errX, errXPrevious, X);
+             
+             errY = getErr(350, y_positionClean);
+             lcd_locate(0, 7);
+             lcd_printf("ErrY = %04d", errY);
+             
+             PD_Conctroller_Axis(errY, errYPrevious, Y);
+
+             
+            flagServo = 0;
+         }
+        
+        } 
+        
 }
+
