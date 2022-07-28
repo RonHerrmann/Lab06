c5340edda628a0e1feb1c0802641f3e80e2284e2
RonHerrmann <84956778+RonHerrmann@users.noreply.github.com>
2022-07-28 14:06:20 +0200

    Update lab06_servos.c

--- Sessio3_Group2_Boyang_Ron_Lab06_Amazing_Ball.X/Lab06-main/Lab06_Amazing_Ball.X/src/lab06_servos.c
+++ Sessio3_Group2_Boyang_Ron_Lab06_Amazing_Ball.X/Lab06-main/Lab06_Amazing_Ball.X/src/lab06_servos.c
@@ -1,77 +1,312 @@
+#include "lab06.h"
 #include "lab06_servos.h"
+#include "lab06_touchscreen.h"
 
 #include <xc.h>
 //do not change the order of the following 2 definitions
 #define FCY 12800000UL
 #include <libpic30.h>
+#include <stdint.h>
+
+#include <math.h>
 
 #include "types.h"
 #include "lcd.h"
-
-#define X 0
-#define Y 1
+#include "led.h"
 
 
 /*
- * Function to initilize servo
- * 
- * input: servo x or y
+ * Parameter
  */
 
-void servo_initialization(uint8_t servo)
-{
-    //setup timer 2
-    CLEARBIT(T2CONbits.TON); // Disable Timer
-    CLEARBIT(T2CONbits.TCS); // Select internal instruction cycle clock
-    CLEARBIT(T2CONbits.TGATE); // Disable Gated Timer mode
-	
-    TMR2 = 0x00; // Clear timer register
-    T2CONbits.TCKPS = 0b10; // Select 1:64 Prescaler
-	
-    CLEARBIT(IFS0bits.T2IF); // Clear Timer2 interrupt status flag
-    CLEARBIT(IEC0bits.T2IE); // Disable Timer2 interrupt enable control bit
-	
-    PR2 = 4000; // Set timer period 20ms:    // 4000= 20*10^-3 * 12.8*10^6 * 1/64
-	
-    if(servo == X){
-		
-        //setup OC8
-		
-        CLEARBIT(TRISDbits.TRISD7); /* Set OC8 as output */
-        OC8R = 3700; 
-        OC8RS = 3700; 
-        OC8CON = 0x0006; /* Set OC8: PWM, no fault check, Timer2 because the 4th bit is 0(1for timer3)*/
-        
-    }else if(servo == Y){
-		
-        //setup OC7
-		
-        CLEARBIT(TRISDbits.TRISD6); /* Set OC7 as output, */
-        OC7R = 3700; 
-        OC7RS = 3700; 
-        OC7CON = 0x0006; /* Set OC8: PWM, no fault check, Timer2 because the 4th bit is 0(1for timer3)*/
-    }
+#define pi 3.14159265
+
+#define X 0
+#define Y 1
+#define x_Axis 0
+#define y_Axis 1
+
+
+#define getErr(Soll, positionClean) (int16_t)(positionClean - Soll)
+#define getPR3(sampleRate) ((float) 12.8e6 * 1/sampleRate / 256)
+#define getTime(counter) ((float) counter * 10 / 1000)
+#define getOmega(frequency) ((float) 2 * pi * frequency)
+
+
+#define radius 100
+#define speed  1 // Hz
+#define centerX 443
+#define centerY 346
+
+
+/*
+ * Common Definitions
+ */
+#define TCKPS_1   0x00
+#define TCKPS_8   0x01
+#define TCKPS_64  0x02
+#define TCKPS_256 0x03
+
+
+/*
+ * Generate Set Point
+ */
+   
+#define generateCircleCoordinateX(center, radius, speed, counterTime) (uint16_t) (center + radius * sinf(getOmega(speed) * getTime(counterTime)))
+#define generateCircleCoordinateY(center, radius, speed, counterTime) (uint16_t) (center + radius * cosf(getOmega(speed) * getTime(counterTime)))
     
-    SETBIT(T2CONbits.TON); /* Turn Timer 2 on */
+   
+/*
+ * Butterworth Filter N=1, Cutoff 3 Hz, sampling @ 50 Hz
+ */
+
+#define b1 0.1602
+#define b2 0.1602
+#define a1 1
+#define a2 -0.6796
+
+#define ButterworthFilter(position, positionPrevious, positionPreviousClean) (b1 * position + b2 * positionPrevious - a2 * positionPreviousClean)
+    
+
+
+/*
+ * Global Variables
+ */
+
+uint8_t volatile dimension = 0;
+uint16_t volatile counter = 0;
+uint16_t volatile iInterrupt = 0;
+uint16_t volatile InterruptDeadline = 0;
+uint16_t volatile deadline = 0;
+uint8_t volatile flagTouch = 0;
+uint8_t volatile flagServo = 0;
+
+
+/*
+ * Timer Code
+ */
+
+void initialize_timer(){
+    
+
+/*****setup Timer 1 *****/
+    
+ T1CONbits.TON = 0; // Disable the Timers
+ T1CONbits.TCKPS = 0b10; // Set Prescaler 64
+ T1CONbits.TCS = 0; // Set Clock Source (external = 1)
+ T1CONbits.TGATE = 0; // Set Gated Timer Mode -> don't use gating //this line can be ignored, if TCS =  1 (have a look at the manual)  
+ T1CONbits.TSYNC = 1; 
+ PR1 = 2000; // Set timer period 10ms (100 Hz):    // 2000= 10*10^-3 * 12.8*10^6 * 1/64 
+ TMR1 = 0x00; // Reset Timer Values
+ 
+ /* Insert Priority here!!! */
+ IPC0bits.T1IP = 0x01; // Set Interrupt Priority (actually Level 1)
+ 
+ IFS0bits.T1IF = 0; // Clear Interrupt Flags
+ IEC0bits.T1IE = 1; // Enable Interrupts
+ T1CONbits.TON = 1; // Enable the Timers    
+    
 }
 
 
 /*
- * Function to set servo to needed PWM signal
- * 
- * input: servo x or y, needed duty cycle of PMW signal
+ * PD Controller
  */
 
-void set_dutyCycle(uint8_t servo, float dutyCycle){
+#define Kp 0.105  //0.083 //0.088   
+#define Kd 0.0735  //0.061 //0.0735   
 
-    if(servo == X){
-        //setup OC8
-        OC8RS = (uint16_t)(4000-dutyCycle*200); /* Load OCRS: next pwm duty cycle , 4000-XX because of inversion*/
-          
-    }else if(servo == Y){
-        //setup OC7
-        OC7RS = (uint16_t)(4000-dutyCycle*200); /* Load OCRS: next pwm duty cycle */
+//#define setPointServoX 1.64
+//#define setPointServoY 1.44
+
+#define setPointServoX 3680
+#define setPointServoY 3717
+
+#define dutyCycle(errLocal, errPreviousLocal, setPoint) (uint16_t)((Kp * errLocal + Kd * (errLocal - errPreviousLocal)/0.02) + setPoint)
+
+   
+/*
+ * ISR code 
+ * 
+ * Note: Timer 2 is already used for servos
+ */
+
+void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T1Interrupt(void)   // read  the touchscreen X and Y dimension (read either X or Y and than alternate between them)
+{   
+    //TOGGLELED(LED1_PORT);
+    
+    if(InterruptDeadline == 1){  
+        deadline++;
+//        InterruptDeadline = 0;
     }
+    
+    flagTouch =1;   // 100 Hz
+   
+    
+    if(iInterrupt == 1){  // 50 Hz
+      flagServo = 1; 
+      iInterrupt = 0;  
+    }
+    
+    counter++;  // 10 ms
+    
+    IFS0bits.T1IF = 0;      // clear interrupt service routine flag
+}
 
+
+
+/*
+ * main loop
+ */
+void main_loop()
+{
+    // print assignment information
+    lcd_printf("Lab06: Amazing Ball");
+    lcd_locate(0, 1);
+    lcd_printf("Group2: Boyang & Ron");
+    lcd_locate(0, 2);
+    
+    uint16_t  x_positionClean = 0;
+    uint16_t  y_positionClean = 0;
+    uint16_t  x_position = 0;
+    uint16_t  y_position = 0;
+    uint16_t  x_positionPrevious = 0;
+    uint16_t  y_positionPrevious = 0;
+    uint16_t  x_positionPreviousClean = 0;
+    uint16_t  y_positionPreviousClean = 0;
+    uint16_t  setPointX = 0;
+    uint16_t  setPointY = 0;
+    uint16_t  dutyCycle = 0;
+    
+    int16_t  errX = 0;
+    int16_t  errXPrevious = 0;
+    int16_t  errY = 0;
+    int16_t errYPrevious = 0;
+    
+    
+   // initialize timers
+    initialize_timer();
+    
+    // initialize touchscreen
+    initialize_touchscreen();
+    
+    // initialize servos
+    servo_initialization(X);
+    servo_initialization(Y);
+   __delay_ms(2000);
+    
+    int main_Counter = 0;
+    
+    while(TRUE) {
+        
+//        lcd_locate(0, 3);
+//        lcd_printf("Counter = %3u", counter)
+        lcd_locate(0, 7);
+        lcd_printf("Deadline misses = %3u", deadline);
+        
+        
+        if(flagTouch == 1){     // 100Hz
+            main_Counter ++;
+            InterruptDeadline = 1;
+            iInterrupt ++;
+            
+            switch(dimension) {
+                case 0: 
+                        x_positionPrevious = x_position;
+                        x_position = currentBallPosition(X);
+                        changeDimension_touchscreen(Y); // BZ:19.07: diese Funktion braucht zu viel Zeit führt zu deadline misses!! delay zu viel in der fkt!
+//                        lcd_locate(0, 3);
+//                        lcd_printf("X gemessen = %3u", x_position);
+                        dimension = 1; 
+				break;
+                case 1: 
+                        y_positionPrevious = y_position;
+                        y_position = currentBallPosition(Y);
+                        changeDimension_touchscreen(X); // BZ:19.07: diese Funktion braucht zu viel Zeit führt zu deadline misses!! delay zu viel in der fkt!
+//                        lcd_locate(0, 4);
+//                        lcd_printf("Y gemessen = %3u", y_position);
+                        dimension = 0;
+				break;
+                }
+
+            InterruptDeadline = 0;
+            flagTouch = 0;
+            
+        } 
+        
+//        lcd_locate(0, 3);
+//        lcd_printf("main_counter = %u", main_Counter);
+        
+
+        if(flagServo == 1){  //50 Hz
+             
+            setPointX = generateCircleCoordinateX(centerX, radius, speed, counter);			// generte circle coordinates
+            setPointY = generateCircleCoordinateY(centerY, radius, speed, counter);
+              
+
+            x_positionClean = ButterworthFilter(x_position, x_positionPrevious, x_positionPreviousClean);
+            x_positionPreviousClean = x_positionClean;
+//            lcd_locate(0, 5);
+//            lcd_printf("X = %3u", x_positionClean); 
+            errXPrevious = errX;
+            errX = getErr(setPointX, x_positionClean); 
+//            errX = getErr(centerX, x_positionClean); 	// setPoint middle X
+//            lcd_locate(0, 6);
+//            lcd_printf("ErrX = %3i", errX);
+//             lcd_locate(0, 6);
+//             lcd_printf("X_circle = %03d", setPointX);
+
+             
+             
+            dutyCycle = dutyCycle(errX, errXPrevious, setPointServoX);   // servo operation                         
+
+            if(dutyCycle <= 3560){    //implement the allowed Range for manipulation of the servo. //2.24
+                dutyCycle = 3560;
+            }else if(dutyCycle >= 3800){   //1.04
+                    dutyCycle = 3800;
+                 }
+
+                      
+//            lcd_locate(0, 4);
+//            lcd_printf("DutyCycle X = %4u", dutyCycle);
+            set_dutyCycle(X, dutyCycle);
+		
+             
+
+            y_positionClean = ButterworthFilter(y_position, y_positionPrevious, y_positionPreviousClean);
+            y_positionPreviousClean = y_positionClean;
+//            lcd_locate(0, 6);
+//            lcd_printf("Y = %3u", y_positionClean);
+            errYPrevious = errY;
+            errY = getErr(setPointY, y_positionClean); 
+//            errY = getErr(centerY, y_positionClean); // set Point middle y     	
+//            lcd_locate(0, 7);
+//            lcd_printf("ErrY = %3i", errY);
+//            lcd_locate(0, 7);
+//            lcd_printf("Y_circle = %03d", setPointY);
+
+
+             
+            dutyCycle = dutyCycle(errY, errYPrevious, setPointServoY);	 // servo operation
+
+            if(dutyCycle <= 3597){    //implement the allowed Range for manipulation of the servo. //2.04
+                dutyCycle = 3597;
+            }else if(dutyCycle >= 3837){  //0.84
+                dutyCycle = 3837;
+            }
+
+                       
+//            lcd_locate(0, 5);
+//            lcd_printf("DutyCycle Y = %4u", dutyCycle);
+            set_dutyCycle(Y, dutyCycle);
+
+            
+            
+            flagServo = 0;
+            
+         }
+        
+    } 
+        
 }
 
